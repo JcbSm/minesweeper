e2caa41776a9be4cd452bc74377a681d4329d9cd
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-10-31 18:55:05 +0000

    v2

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -5,13 +5,29 @@ import java.awt.*;
 
 public class Cell extends JButton {
 
-    /**
-     * State: 0 - Unclicked, 1 - Empty, 2 - Adjacent, 3 - Mine
-     */
-    public int state = 0;
+    public int row;
+    public int column;
 
-    public Cell() {
-        this.setSize(25, 25);
+    /**
+     * State: 0 - Unclicked; 1 - Clicked; 2 - Flagged
+     */
+    private int state = 0;
+    private int adjacentMines = 0;
+    private boolean mine;
+
+    /**
+     * Creates a new cell
+     * @param i the row of the cell
+     * @param j the column of the cell
+     */
+    public Cell(int i, int j, boolean mine) {
+
+        row = i;
+        column = j;
+
+        state = mine ? 2 : 0;
+
+        this.setSize(50, 50);
         this.setBackground(Color.LIGHT_GRAY);
         this.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
     }
@@ -24,12 +40,25 @@ public class Cell extends JButton {
         this.setText(String.valueOf(count));
     }
 
-    public void setEmpty() {
-
+    /**
+     * Checks if the cell has a mine
+     * @return boolean
+     */
+    public boolean isMine() {
+        return mine;
     }
 
-    public void setMine() {
-
+    /**
+     * Sets the value for adjacent mines
+     * @param count adjacent mines count
+     * @return int - count
+     */
+    public int setAdjacentMines(int count) {
+        return adjacentMines = count;
     }
 
+    public void plantMine() {
+        mine = true;
+        this.setBackground(Color.RED);
+    }
 }
--- src/game/Minesweeper.java
+++ src/game/Minesweeper.java
@@ -2,4 +2,12 @@ package src.game;
 
 public class Minesweeper {
 
+    public static void main(String[] args) {
+
+        Board board = new Board(10, 10, 70);
+
+        board.frame.setVisible(true);
+
+    }
+
 }
